5b94a2e5d478dcaccb32b044f0b07496a029237b
unknown <neilkamath07@gmail.com>
2024-05-29 21:04:56 -0700

    initial

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string myName = "Neil";
+    int myAge = 75;
+
+    cout << "Hello world!" << endl;
+    cout << "My name is " << myName << endl;
+    cout << "I am " << myAge << " years old." << endl;
+
+    return 0;
+}
